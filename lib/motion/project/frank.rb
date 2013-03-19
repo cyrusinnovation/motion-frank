889756277a3c1435337008dbcac4008348940f54
Paul Infield-Harm <paul@infield-harm.org>
2013-03-18 20:28:55 -0400

    First approximately working version
    - rake frank:init_features will create starter features
    - rake frank:run will run features
    - rake frank:symbiote will run the simulator with symbiote enabled
    
    Note: frank:symbiote will leave behind the symbiote instrumentation
    so that subsequent simulator runs will run symbiote. There's a bug
    in how the build system cleans up resources that causes this. If
    it doesn't get fixed I'll add something neater later.
    
    I'm tracking the version of frank-cucumber with the versions of this,
    so that e.g. 1.1.8.* correspond to frank-cucumber version 1.1.8

--- lib/motion/project/frank.rb
+++ lib/motion/project/frank.rb
@@ -1,3 +1,48 @@
 unless defined?(Motion::Project::Config)
   raise "This file must be required within a RubyMotion project Rakefile."
 end
+
+require 'cucumber/rake/task'
+
+frank_skeleton = File.join(Gem.loaded_specs['frank-cucumber'].full_gem_path, 'frank-skeleton')
+
+Motion::Project::App.setup do |app|
+  app.vendor_project(frank_skeleton, :static)
+  app.frameworks << 'CFNetwork'
+end
+
+namespace 'frank' do
+
+  desc 'Build app for simulator, launch simulator and run features'
+  task :run do
+    Rake::Task["build:simulator"].invoke
+    Cucumber::Rake::Task.new(:features, "") do |t|
+      app_bundle_path = File.expand_path(App.config.app_bundle('iPhoneSimulator'), Rake.application.original_dir)
+      t.cucumber_opts = "APP_BUNDLE_PATH='#{app_bundle_path}' --format pretty #{(ENV["FEATURES"] || "features")} "
+    end.runner.run
+  end
+
+  desc "Run app with symbiote enabled"
+  task :symbiote do
+    Motion::Project::App.setup do |app|
+      app.resources_dirs << frank_skeleton
+    end
+    Rake::Task["simulator"].invoke
+  end
+
+  desc "Create first feature files"
+  task :init_features do
+    target_dir = File.join(Rake.application.original_dir, 'features')
+    unless File.exists?(target_dir)
+      FileUtils.cp_r(File.join(frank_skeleton, 'features'), target_dir)
+      puts "created new features in ./features"
+    else
+      puts "features/ already exists, skipping."
+    end
+  end
+
+end
+
+desc "Shorthand for frank:run"
+task 'frank' => 'frank:run'
+
--- motion-frank.gemspec
+++ motion-frank.gemspec
@@ -1,14 +1,13 @@
 # -*- encoding: utf-8 -*-
 
-Version = '0.0.1'
-
 Gem::Specification.new do |s|
-  s.name        = "motion-frank"
-  s.version     = Version
-  s.summary     = "Frank integration for RubyMotion projects"
-  s.description = "motion-frank provides easy integration of frank-cucumber for in-simulator testing"
+  s.name        = 'motion-frank'
+  s.version     = '1.1.8.0'
+  s.summary     = 'Frank integration for RubyMotion projects'
+  s.description = 'motion-frank provides easy integration of frank-cucumber for in-simulator testing'
   s.author      = 'Paul Infield-Harm'
   s.email       = 'pinfield-harm@cyrusinnovation.com'
+  s.add_dependency 'frank-cucumber', '1.1.8'
 
   files = []
   files << 'README.rdoc'
